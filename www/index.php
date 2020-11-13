aa3227f8eb8c23feb64c2e2175f35c59c6c855d4
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-11-12 17:12:04 -0800

    chore(ci): hello world in tests

--- www/index.php
+++ www/index.php
@@ -1,2 +1,15 @@
-<?php
-  echo "A";
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Page Title</title>
+
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p id="hello">This is a paragraph.</p>
+
+
+</body>
+</html>
