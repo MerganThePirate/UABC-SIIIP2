9cd7412f9b1fa94e480ec111adf2d85b436169d4
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-11-13 11:52:47 -0800

    chore(ci): add docker to github action

--- tests/framework/webapp.py
+++ tests/framework/webapp.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 from data.config import settings
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class WebApp:
@@ -15,9 +15,11 @@ class WebApp:
         return cls.instance
 
     def __init__(self):
-       chrome_options = Options()
-       chrome_options.add_argument("--headless")
-       self.driver = webdriver.Chrome(options=chrome_options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
     def get_driver(self):
         return self.driver
