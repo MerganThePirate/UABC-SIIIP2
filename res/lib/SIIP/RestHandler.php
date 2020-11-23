5a7d46ec217b38e3d0610c91ebb2072f63233b01
David <dmgf2008@hotmail.com>
2020-11-23 10:17:17 -0800

    Changes to RestHandler & htaccess to use Abstract Handler Class

--- res/lib/SIIP/RestHandler.php
+++ res/lib/SIIP/RestHandler.php
@@ -1,30 +1,24 @@
 <?php
 /** Create $_PUT & $_DELETE Arrays */
 require_once(__DIR__."/Requests.php");
+header('Content-type: application/json; charset=utf-8');
 
+function sendResponse($code, $data) {
+  http_response_code($code);
+  echo $data;
+}
+
+$response = ["code" => "404", "data" => "{}"];
 /** Evaluate URI */
 /** /usuarios/n */
-preg_match('#(usuarios)/([0-9]*)#', $_SERVER["REQUEST_URI"], $match);
-
-/** Inspect URI first, then inspect Request Method and perform instructions */
-switch($match[1]) {
-  case "usuarios":
-    switch($_SERVER['REQUEST_METHOD']) {
-      case "GET":
-        /** User Handler */
-        require(__DIR__."/MAS/Usuario.php");
-        /** One User */
-        if (is_numeric($match[2])) {
-          Usuario::getUser($match[2]);
-        /** Else All Users */
-        } else {
-          Usuario::getAllUsers();
-        }
-        break;
-      case "POST":
-      case "PUT":
-      case "DELETE":
-    }
-    break;
+if (preg_match('#/(usuarios)/([0-9]*)$#', $_SERVER["REQUEST_URI"], $match)) {
+  require(__DIR__."/MAS/Usuario.php");
+  $response = Usuario::processRequestMethod($_SERVER['REQUEST_METHOD'],
+    [
+      "URI" => $match[0],
+      "user_id" => $match[2]
+    ]
+  );
 }
+sendResponse($response["code"], $response["data"]);
 ?>
